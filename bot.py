f4d057d1b04aa57ce2049cf130d1542f5fc389d6
H3ndrik1337 <36861180+H3ndrik1337@users.noreply.github.com>
2018-05-15 15:23:33 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -124,7 +124,50 @@ async def on_message(message):
     if message.content.startswith('!uptime'):
         await client.send_message(message.channel, "**Ich bin schon {0} Stunde/n und {1} Minuten online auf {2}. **".format(hour, minutes, message.server))
 
+@client.event
+async def on_message(message):
+    if message.content.startswith('!join'):
+        try:
+            channel = message.author.voice.voice_channel
+            await client.join_voice_channel(channel)
+        except discord.errors.InvalidArgument:
+            await client.send_message(message.channel, "Ich habe keinen Voicechannel gefunden.")
+        except Exception as error:
+            await client.send_message(message.channel, "Ein Error: ```{error}```".format(error=error))
 
+    if message.content.startswith('!quit'):
+        try:
+            voice_client = client.voice_client_in(message.server)
+            await voice_client.disconnect()
+        except AttributeError:
+            await client.send_message(message.channel, "-No Channel-")
+        except Exception as Hugo:
+            await client.send_message(message.channel, "Ein Error: ```{haus}```".format(haus=Hugo))
+
+    if message.content.startswith('!play '):
+        try:
+            yt_url = message.content[6:]
+            channel = message.author.voice.voice_channel
+            voice = await client.join_voice_channel(channel)
+            player = await voice.create_ytdl_player(yt_url)
+            players[message.server.id] = player
+            player.start()
+        except:
+            await client.send_message(message.channel, "Error.")
+
+    if message.content.startswith('!pause'):
+        try:
+            players[message.server.id].pause()
+        except:
+            pass
+    if message.content.startswith('!resume'):
+        try:
+            players[message.server.id].resume()
+        except:
+            pass
+        
+        
+        
 
 async def total_uptime():
     await client.wait_until_ready()
